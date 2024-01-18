8dd59bdcb9481191b86371d6b91021ab388746f5
adricastro1 <adricastro1196@gmail.com>
2024-01-18 10:25:00 -0500

    Hero section buttons and video background

--- react-website-practice/src/Components/HeroSection.js
+++ react-website-practice/src/Components/HeroSection.js
@@ -1,4 +1,7 @@
 import React from "react";
+import '../App.css'
+import { Button } from "./Button";
+import './HeroSection.css'
 
 function HeroSection () {
     return (
@@ -6,6 +9,11 @@ function HeroSection () {
             <video src="/videos/video-2.mp4" autoPlay loop muted/>
             <h1>Adventure Awaits</h1>
             <p>What are you waiting for?</p>
+            <div className="hero-btns">
+                <Button className="btns" buttonStyle="btn--outline" buttonSize="bttn--large"></Button>
+                <Button className="btns" buttonStyle="btn--primary" buttonSize="bttn--large">WATCH TRAILER <i className="far fa-play-circle"/></Button>
+
+            </div>
         </div>
     )
 }
